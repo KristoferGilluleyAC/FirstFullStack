50a956b4e54c8418b6aee79deddcaf827b6e86aa
Kristofer Gilluley <kristofer.gilluley@arnoldclark.com>
2022-08-10 12:52:12 +0100

    :sparkles: Dashboard: Impemented

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { connect } from "react-redux";
 import { Dashboard } from './Dashboard';
 
-export const TaskList = ({tasks}) => (
+export const TaskList = ({tasks, name}) => (
     <div>
-        {tasks.map(task=>(<div>{task.name}</div>))}
+        <h3>
+            {name}
+        </h3>
+        <div>
+            {tasks.map(task=>(<div>{task.name}</div>))}
+        </div>
     </div>
 )
 
